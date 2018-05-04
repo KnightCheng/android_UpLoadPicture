3562f98a690881f021cffa678182a9cf0869e79c
Knight <1105537272@qq.com>
2018-05-04 15:08:36 +0800

    start
    start

---
+++ PictureUpLoad/app/src/main/java/com/example/knight/pictureupload/FileUtils.java
@@ -0,0 +1,11 @@
+package com.example.knight.pictureupload;
+
+import android.os.Environment;
+
+/**
+ * Created by yg_DC on 2018/5/4.
+ */
+
+public class FileUtils {
+    public static String SDPATH= Environment.getExternalStorageDirectory()+"/TEST_PY/";
+}
